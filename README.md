e5f20f10d3697992092c38835c78bdd7b2704c0a
Simon Pither <simon@pither.com>
2019-06-21 16:29:30 +0100

    The state after the 2019-06-20 Red Sprite hacking

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dingdong
 The Ding Dong mobile app(s) - please create a directory per implementation and update the README
+
+Directory | Notes / status
+--------- | --------------
+simon | Tested via Android emulator, includes fix for permission to read contacts, currently fails trying to send an SMS, not very well styled!
+
--- simon/DingDong/App.js
+++ simon/DingDong/App.js
@@ -1,15 +1,80 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import * as Permissions from 'expo-permissions';
+import * as SMS from 'expo-sms';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends React.Component {
+  state = {
+    contacts: []
+  };
+
+  componentDidMount() {
+    this.getContacts();
+  }
+
+  getContacts = async () => {
+    const { status, permissions } = await Permissions.askAsync(Permissions.CONTACTS);
+    const { data } = await Contacts.getContactsAsync({
+      fields: [Contacts.Fields.PhoneNumbers],
+    });
+
+    this.setState({ contacts: data })
+  };
+
+  handleContactPressed = async contact => {
+    console.log('you pressed on: ', contact.name, contact.phoneNumbers[0].digits);
+    // Check if SMS is available
+    const isAvailable  = await SMS.isAvailableAsync();
+    if (isAvailable) {
+      // We can send a text!
+      const result = await SMS.sendSMSAsync(
+          [contact.phoneNumbers[0].digits],
+          "DING DONG - I'm here!"
+      );
+    } else {
+
+    }
+  };
+
+  render() {
+    return (
+        <SafeAreaView style={styles.container}>
+          <FlatList
+            style={{ flex: 1 }}
+            data={this.state.contacts}
+            renderItem={({item}) => (
+                <TouchableOpacity
+                  onPress={() => this.handleContactPressed(item)}
+                  style={styles.button}
+                  >
+              <Text key={item.key} style={styles.text}>
+                {item.name}
+              </Text>
+                </TouchableOpacity>
+            )}
+          />
+        </SafeAreaView>
+    );
+  }
 }
 
+export default App;
+
 const styles = StyleSheet.create({
+  button: {
+    marginVertical: 10,
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: 'black',
+    width: '100%'
+  },
+  text: {
+    height: 40,
+    fontSize: 20,
+    color: '#FFF',
+    textAlign: 'center'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
